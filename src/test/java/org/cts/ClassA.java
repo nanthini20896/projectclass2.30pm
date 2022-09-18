965f1164d93700cc30e27ec3f0d24cda676f3cec
nanthini20896 <nanthini20896@gmail.com>
2022-09-18 23:43:15 +0530

    Added 3 methods

--- src/test/java/org/cts/ClassA.java
+++ src/test/java/org/cts/ClassA.java
@@ -1,5 +1,35 @@
 package org.cts;
 
 public class ClassA {
+	
+	private void Method1() {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+	}
+	
+	private void Method2() {
+		// TODO Auto-generated method stub
+		System.out.println("2");
+	}
+	
+	private void Method3() {
+		// TODO Auto-generated method stub
+		System.out.println("3");
+	}
+	
+	private void Ramnad() {
+		// TODO Auto-generated method stub
+		System.out.println("Ramnad");
+	}
+	
+	private void Theni() {
+		// TODO Auto-generated method stub
+		System.out.println("Theni");
+	}
+	
+	private void Madurai() {
+		// TODO Auto-generated method stub
+		System.out.println("Madrai");
+	}
 
 }
