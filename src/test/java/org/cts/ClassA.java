6059782e7118709411372577ef950cad16052c34
nanthini20896 <113781604+nanthini20896@users.noreply.github.com>
2022-09-18 23:51:40 +0530

    Update ClassA.java

--- src/test/java/org/cts/ClassA.java
+++ src/test/java/org/cts/ClassA.java
@@ -1,5 +1,23 @@
 package org.cts;
 
 public class ClassA {
+  
+  private void Method1() {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+	}
+	
+	private void Method2() {
+		// TODO Auto-generated method stub
+		System.out.println("2");
+	}
+	
+	private void Method3() {
+		// TODO Auto-generated method stub
+		System.out.println("3");
+	}
+	
+  
+  
 
 }
